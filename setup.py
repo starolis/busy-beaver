c345405577771e8a080a66c5dea59b84ccbcfe0f
Matt Starolis <starolis@gmail.com>
2024-09-14 18:25:43 -0500

    remove multiprocessing req

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         # Core Dependencies
-        "itertools",
-        "multiprocessing",
         "functools",
         # Development Dependencies
         "flake8==6.0.0",
